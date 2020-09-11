bf70c64b74f7dd3de909ca615ea1f73ea3ba6e3f
Kevin Leo <lgxz@users.noreply.github.com>
2020-09-11 14:41:55 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d --rm --network none -v "$(pwd)"/root:/root -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -h $HOSTNAME --name ghidra ghidra:v1
+docker run -d --rm --network none -v "$(pwd)"/root:/root -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -h $HOSTNAME --name ghidra lgxz/ghidra
 
